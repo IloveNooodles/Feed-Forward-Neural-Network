965b4b82b96d032eb07b1b11f302c6630671fcff
IloveNooodles <mgarebaldhie80@gmail.com>
2023-03-30 00:51:44 +0700

    feat: update validation format using numpy

--- src/reader.py
+++ src/reader.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+import numpy as np
 import pandas as pd
 from sklearn import datasets
 
@@ -32,7 +33,7 @@ class Reader:
                 json_file = json.loads(f.read())
                 # Return models
                 if validate_data(json_file):
-                    return json_file["layers"], json_file['activation_functions'], json_file['neurons'], json_file['weights']
+                    return json_file
                 return None
         except OSError as e:
             print("File not found")
@@ -42,48 +43,70 @@ class Reader:
 def validate_data(json_data) -> bool:
     # Validate layers
     layers = json_data['layers']
-    activation_functions = json_data['activation_functions']
-    neurons = json_data['neurons']
-    weights = json_data['weights']
-    if not isinstance(json_data['layers'], int):
-        return False
+    activation_functions = np.array(
+        json_data['activation_functions'], dtype=np.int32)
 
-    # Validate activation function per layers
-    if len(activation_functions) != layers:
-        return False
+    # Neurons are input hidden output
+    neurons = np.array(json_data['neurons'], dtype=np.int32)
+    weights = json_data['weights']
+    rows = json_data['rows']
+    data = np.array(json_data['data'], dtype=np.float64)
+    data_names = np.array(json_data['data_names'], dtype=np.string_)
+    target_names = np.array(json_data['target_names'], dtype=np.string_)
+    target = np.array(json_data['target'], dtype=np.int32)
+
+    if not isinstance(layers, int):
+        raise Exception("Layers is not integer")
+
+    if layers < 3:
+        raise Exception("Layers minimal have value of 3 (input, hidden, end)")
+
+    # # Validate activation function per layers
+    if activation_functions.shape[0] != layers:
+        raise Exception("Length of activation functions is not the same")
+
+    assert activation_functions.dtype == np.int32
 
     for function in activation_functions:
-        if not isinstance(function, int):
-            return False
-
         if function not in ACTIVATION_LIST:
-            return False
+            raise Exception("Invalid exception")
 
-    # Validate neurons
-    if len(neurons) != layers:
-        return False
+    # # Validate neurons
+    if neurons.shape[0] != layers:
+        raise Exception("Neurons number don't match with layers")
 
-    for neuron in neurons:
-        if not isinstance(neuron, int):
-            return False
+    assert neurons.dtype == np.int32
 
-    # Validate weights, weight len must be neuron + 1
-    if len(weights) != layers:
-        return False
+    # Validate weights, weights must be layers - 1
+    if len(weights) != layers - 1:
+        raise Exception("Please input correct weights")
 
-    for index, weight in enumerate(weights):
-        if len(weight) != neurons[index] + 1:
-            return False
+    for index, weight_per_neuron in enumerate(weights):
+        for weight_neuron in weight_per_neuron:
+            if len(weight_neuron) != neurons[index] + 1:
+                raise Exception(
+                    f"Invalid number of weights parameter in weight {index}")
+            np.array(weight_neuron, dtype=np.float64)
 
-        for num in weight:
-            if not isinstance(num, int):
-                return False
+    # Validate rows
+    if not isinstance(rows, int):
+        raise Exception("Rows is not integer")
 
+    # data_names
+    len_data_features = data_names.shape[0]
+    assert np.issubdtype(data_names.dtype, np.string_) == True
+
+    # data attr
+    if data.shape[0] != len_data_features:
+        raise Exception("Number of data doesn't match")
+
+    assert np.issubdtype(target_names.dtype, np.string_) == True
+    assert target.shape[0] == rows
     return True
 
 
 if __name__ == "__main__":
     data = Reader.read_ffnn("./test/test.json")
-    data2 = datasets.load_breast_cancer()
+    # data2 = datasets.load_breast_cancer()
     print(data)
-    print(data2)
+    # print(data2)
