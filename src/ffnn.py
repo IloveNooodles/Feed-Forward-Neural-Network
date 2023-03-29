6c61621ee42b47a16a770d8346cd9f26ec005775
IloveNooodles <mgarebaldhie80@gmail.com>
2023-03-30 00:58:26 +0700

    feat: finish model for ffnn

--- src/ffnn.py
+++ src/ffnn.py
@@ -5,17 +5,33 @@ from reader import Reader
 
 
 class FFNN:
-    def __init__(self, layers: int, activation_functions: List[int], neurons: List[int], weights: List[List[int]]) -> None:
-        self.layers = layers
-        self.activation_functions = activation_functions
-        self.neurons = neurons
-        self.weights = weights
+    def __init__(self, model) -> None:
+        self.layers = model['layers']
+        self.activation_functions = model['activation_functions']
+        self.neurons = model['neurons']
+        self.weights = model['weights']
+        self.rows = model['rows']
+        self.data_names = model['data_names']
+        self.data = model['data']
+        self.target = model['target']
+        self.target_names = model['target_names']
         self.epochs = None
         self.batch_size = None
         pass
 
     def __str__(self) -> str:
-        return f"Layers: {self.layers}\nActivations: {self.activation_functions}\nNeurons: {self.neurons}\nWeights: {self.weights}"
+        return f"\
+  Layers: {self.layers}\n\
+  Activations: {self.activation_functions}\n\
+  Neurons: {self.neurons}\n\
+  Weights: {self.weights}\n\
+  Rows: {self.rows}\n\
+  Data: {self.data}\n\
+  Data_names: {self.data_names}\n\
+  target: {self.target}\n\
+  target_names: {self.target_names}\n\
+  epochs: {self.epochs}\n\
+  batch_size: {self.batch_size}\n"
 
     def set_data():
         pass
@@ -31,10 +47,9 @@ class FFNN:
 
 
 if __name__ == "__main__":
-    layer, activations, neurons, weights = Reader.read_ffnn("./test/test.json")
+    model = Reader.read_ffnn("./test/test.json")
     try:
-        a = FFNN(layers=layer, activation_functions=activations,
-                 neurons=neurons, weights=weights)
+        a = FFNN(model=model)
         print(a)
     except:
-        raise ("Invalid formats")
+        raise Exception("Invalid formats")
